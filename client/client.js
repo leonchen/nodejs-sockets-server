8943fe843fc739e92a24236770de999343a504f6
leonchen <leon@factual.com>
2012-12-10 02:56:39 +0800

    create lib files

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
-var net = require('net');
 var http = require('http');
 var config = require('./config');
+var sockets = require('../lib/sockets');
 
 var options = {
   host: config.server,
@@ -28,37 +28,5 @@ req.write("user="+config.user+"&pass="+config.secret);
 req.end();
 
 var start = function (port) {
-  net.createServer(function (c) {
-    var buff = new Buffer(0);
-    var connected = false;
-    var s = net.createConnection(port, config.server);
-    s.on('connect', function () {
-      connected = true;
-      if (buff.length) s.write(buff);
-    });
-    s.on('end', function () {
-      console.log('client connect end on port '+port);
-    });
-    s.on('close', function () {
-      console.log('client connect close on port '+port);
-    });
-    s.on('drain', function () {
-      console.log('client connect drain on port '+port);
-    });
-    s.on('error', function () {
-      console.log('client connect error on port '+port);
-    });
-
-
-    c.on('data', function (d) {
-      if (connected) {
-        s.write(d);
-      } else {
-        buff += Buffer.concat([buff, d]);;
-      }
-    });
-    s.pipe(c, {end: false});
-  }).listen(config.local, function () {
-    console.log("listening on port:", config.local);
-  });
+  sockets.createForwardServer(config.local, parseInt(port), config.server);
 };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-var net = require('net');
 var config = require('./config');
+var sockets = require('../lib/sockets');
+
 var express = require('express');
 var app = express();
-
-var usingPorts = {};
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -11,8 +10,8 @@ app.use(app.router);
 
 app.post("/requestSocket", function(req, res) {
   try {
-    var port = getAvailablePort();
-    openSocket(port);
+    var port = sockets.getAvailablePort(config.portsRange[0], config.portsRange[1]);
+    sockets.createForwardServer(port, config.connect);
     res.send(port.toString());
   } catch (e) {
     console.log("failed to bind on port ", port, ":", e);
@@ -24,45 +23,4 @@ app.get("/stats", function (req, res) {
   res.send("TODO: show stats info");
 });
 
-app.listen(config.serverPort);
-
-
-function getAvailablePort() {
-  for (var s=config.portsRange[0], e=config.portsRange[1]; s<=e; s++) {
-    if (usingPorts[s]) continue;
-    try {
-      var ts = net.createServer().listen(s);
-      ts.close();
-      return s;
-    } catch (e) {
-      continue;
-    }
-  }
-}
-
-function openSocket(port) {
-  net.createServer(function (s) {
-    s.on('connect', function () {
-      console.log('connect successfully on port '+port);
-    });
-    s.on('data', function (d) {
-      s.write("server get:"+d);
-    });
-    s.on('end', function () {
-      console.log('server connect end on port '+port);
-    });
-    s.on('close', function () {
-      console.log('server connect close on port '+port);
-    });
-    s.on('drain', function () {
-      console.log('server connect drain on port '+port);
-    });
-    s.on('error', function () {
-      console.log('server connect error on port '+port);
-    });
-    s.on('timeout', function () {
-      console.log('server connect timeout on port '+port);
-    });
-
-  }).listen(port);
-}
+app.listen(config.listen);
